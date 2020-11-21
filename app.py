700b75ad21451ad4413e6021e9d45037a42d372e
rjyobal <rafaelyobal@msn.com>
2020-11-20 18:46:49 -0600

    Complete routes

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ Station = Base.classes.station
 app = Flask(__name__)
 
 #Flask Routes
+# Route ROOT
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -29,10 +30,11 @@ def welcome():
         f'<a href="/api/v1.0/precipitation">/api/v1.0/precipitation</a><br/>'
         f'<a href="/api/v1.0/stations">/api/v1.0/stations</a><br/>'
         f'<a href="/api/v1.0/tobs">/api/v1.0/tobs</a><br/>'
-        f'<a href="/api/v1.0/start">/api/v1.0/start</a><br/>'
-        f'<a href="/api/v1.0/start/end">/api/v1.0/start/end</a><br/>'
+        f'<a href="/api/v1.0/start">/api/v1.0/start</a> Replace "start" with date: [yyyy-mm-dd]<br/>'
+        f'<a href="/api/v1.0/start/end">/api/v1.0/start/end</a> Replace "start & end" with dates: [yyyy-mm-dd]<br/>'
     )
 
+# Route Precipitations
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create session
@@ -60,30 +62,33 @@ def precipitation():
         })
     return jsonify(precipitations)
 
+# Route Stations
 @app.route("/api/v1.0/stations")
 def station():
     # Create session
     session = Session(engine)
     # Build Query
     results = (session
-               .query(Measurement.station.distinct())
+                .query(Measurement.station.distinct(), Station.name)
+                .filter(Measurement.station==Station.station)
                .all()
           )
     session.close()
     #Create dictionary
     stations = []
-    for station in results:
+    for station, name in results:
         stations.append({
-            "station name": station
+            "station": station,
+            "station name": name            
         })
     return jsonify(stations)
 
+# Route Temperature Observations
 @app.route("/api/v1.0/tobs")
 def tob():
     # Create session
     session = Session(engine)
     # Build Query
-
     active_station = (session
                       .query(Measurement.station, func.count(Measurement.tobs))
                       .group_by(Measurement.station)
@@ -91,7 +96,6 @@ def tob():
                       .first()
                       .station
                  )
-
     last_date = (session
                  .query(Measurement.date)
                  .order_by(Measurement.date.desc())
@@ -100,14 +104,12 @@ def tob():
     last_date = last_date.date
     year, month, day = last_date.split('-')
     year_ago = dt.date(int(year)-1,int(month),int(day))
-
     results = (session
                .query(Measurement.date, Measurement.tobs)
                .filter(Measurement.station==active_station)
                .filter(Measurement.date>=year_ago)
                .all()
           )
-
     session.close()
     #Create dictionary
     tobs_dict = []
@@ -117,6 +119,50 @@ def tob():
         })
     return jsonify(tobs_dict)
 
+# Route Start Date
+@app.route("/api/v1.0/<start>")
+def startdate(start):
+    # Create session
+    session = Session(engine)
+    # Build Query
+    results = (session
+               .query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs))
+               .filter(Measurement.date>=start)
+               .all()
+          )
+    session.close()
+    #Create dictionary
+    temps = []
+    for m_min, m_max, m_avg in results:
+        temps.append({
+            "Min": m_min,
+            "Max": m_max,
+            "Avg": m_avg
+        })
+        return jsonify(temps)
+
+# Route Start Date & End Date
+@app.route("/api/v1.0/<start>/<end>")
+def startenddate(start, end):
+    # Create session
+    session = Session(engine)
+    # Build Query
+    results = (session
+               .query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs))
+               .filter(Measurement.date>=start)
+               .filter(Measurement.date<=end)
+               .all()
+          )
+    session.close()
+    #Create dictionary
+    temps = []
+    for m_min, m_max, m_avg in results:
+        temps.append({
+            "Min": m_min,
+            "Max": m_max,
+            "Avg": m_avg
+        })
+        return jsonify(temps)
 
 if __name__ == '__main__':
     app.run(debug=True)
